3e2cd2668e23fe8165511ae35da1ed3ef1008219
anderson675 <cl3844@nyu.edu>
2018-08-25 18:48:51 -0400

    27 / 885

---
+++ leetcodeCSharp/Properties/BaseballGame682.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodeCSharp
+{
+    public class BaseballGame682
+    {
+        public int CalPoints(string[] ops) {
+            var stack = new Stack<int>();
+        
+            foreach(var op in ops)
+            {
+                switch (op)
+                {
+                    case "C":
+                        stack.Pop();
+                        break;
+                    case "D":
+                        stack.Push(2 * stack.Peek());
+                        break;
+                    case "+":
+                        int top = stack.Pop();
+                        int newTop = top + stack.Peek();
+                        stack.Push(top);
+                        stack.Push(newTop);
+                        break;
+                    default:
+                        stack.Push(int.Parse(op));
+                        break;
+                }
+            }
+        
+            var sum = 0;
+        
+            while(stack.Count > 0)
+            {
+                sum += stack.Pop();
+            }
+        
+            return sum;
+        }
+    }
+}
---
+++ leetcodeCSharp/Properties/IslandPerimeter463.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcodeCSharp
+{
+    public class IslandPerimeter463
+    {
+        public int IslandPerimeter(int[,] grid)
+        {
+            int count = 0, connected = 0;
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (grid[i, j] == 1)
+                    {
+                        count++;
+                        if (i != 0 && grid[i - 1,j] == 1)
+                        {
+                            connected++;
+                        }
+
+                        if (j != 0 && grid[i, j - 1] == 1)
+                        {
+                            connected++;
+                        }
+                    }
+                }
+            }
+
+            return 4 * count - 2 * connected;
+        }
+    }
+}
