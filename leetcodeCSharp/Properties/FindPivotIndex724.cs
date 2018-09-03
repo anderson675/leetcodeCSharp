726fd6fc524606ee52336e4fa3994bd086319fa2
anderson675 <cl3844@nyu.edu>
2018-09-02 20:14:00 -0400

    60 / 893

---
+++ leetcodeCSharp/Properties/FindPivotIndex724.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcodeCSharp
+{
+    public class FindPivotIndex724
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int lsum = 0;
+            int rsum = 0;
+            foreach (int num in nums)
+            {
+                rsum += num;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                rsum -= nums[i];
+                if (rsum == lsum) return i;
+                lsum += nums[i];
+            }
+
+            return -1;
+        } 
+    }
+}
---
+++ leetcodeCSharp/Properties/TwoSum1.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcodeCSharp
+{
+    public class TwoSum1
+    {
+        public int[] TwoSum(int[] nums, int target) {
+            var numAndIndex = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                var curNumber = nums[i];
+                var numberA = target - curNumber;
+
+                if (numAndIndex.ContainsKey(numberA))
+                {
+                    return new int[] {numAndIndex[numberA], i};
+                }
+
+                numAndIndex[curNumber] = i;
+            }
+            return null; 
+        } 
+    }
+}
