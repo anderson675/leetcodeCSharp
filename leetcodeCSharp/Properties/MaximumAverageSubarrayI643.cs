2f6fe60363a894b212612f928d81a292a666bb4d
anderson675 <cl3844@nyu.edu>
2018-09-05 20:28:48 -0400

    643 done

---
+++ leetcodeCSharp/Properties/MaximumAverageSubarrayI643.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcodeCSharp
+{
+    public class MaximumAverageSubarrayI643
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            int temp = 0;
+            int total = 0;
+            for (int i = 0; i < k; i++)
+            {
+                temp += nums[i];
+            }
+
+            total = temp;
+            for (int i = k; i< nums.Length; i++)
+            {
+                temp -= nums[i - k];
+                temp += nums[i];
+                if (temp > total) total = temp;
+            }
+
+            return total/1.0/ k;
+        } 
+    }
+}
